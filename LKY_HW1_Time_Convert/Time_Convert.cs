f4d5cb141555ed2db061f183d986f67d3b96ca6e
liukaiyuan <liukaiyuan@sjtu.edu.cn>
2022-10-11 16:17:30 +0800

    添加项目文件。

---
+++ LKY_HW1_Time_Convert/Time_Convert.cs
@@ -0,0 +1,80 @@
+﻿/*
+ *      [Principle of Navigation] Copyright (C) 2022 SJTU Inc.
+ *      
+ *      FileName : Time_Convert_LKY
+ *      Developer: liukaiyuan (Odysseus.Yuan)
+ */
+
+using System;
+
+namespace Time_Convert_LKY
+{
+    internal class Time_Convert
+    {
+        static void Main(string[] args)
+        {
+            //欢迎话术
+            Console.WriteLine("******** Homework #1: Time Conversion  ********");
+            Console.WriteLine("******** Developer: Liu Kaiyuan        ********");
+            Console.WriteLine("******** Student Number: 122413930100 ********");
+            Console.WriteLine();
+
+            DateTime time_shanghai = new DateTime(2022, 9, 19, 10, 0, 0);
+            Console.WriteLine("转换上海时间（" + time_shanghai.ToString() +  "）:\n");
+
+            //转换至UTC时间，东八区时间-8小时，得到UTC时间
+            DateTime UTC = time_shanghai.AddHours(-8);
+            Console.WriteLine("---> UTC 时间：" + UTC.ToString());
+
+            //先转换至TAI时间，根据2017年的跳秒资料，TAI = UTC + 37s
+            DateTime TAI = UTC.AddSeconds(37);
+            //TAI转为GPS时间，GPS = TAI - 19s
+            DateTime GPST = TAI.AddSeconds(-19);
+
+            //Console.WriteLine("---> GPS 时间：" + GPS.ToString());
+            Console.WriteLine("---> GPS 时间：" + Get_GNSS_Time(GNSS_Type.GPS, GPST));
+
+            //GPS转为BDS时间，BDT = GPST - 14s
+            DateTime BDT = GPST.AddSeconds(-14);
+            Console.WriteLine("---> BDS 时间：" + Get_GNSS_Time(GNSS_Type.BDS, BDT));
+
+            Console.WriteLine("\nDone. 可按任意键退出运行。");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 获取GNSS格式的时间
+        /// </summary>
+        /// <returns></returns>
+        private static string Get_GNSS_Time(GNSS_Type type, DateTime dateTime)
+        {
+            DateTime startTime;     //定义GNSS的起始发射时间
+
+            //依据不同的GNSS来指定起始时间
+            if (type == GNSS_Type.GPS)
+            {
+                startTime = new DateTime(1980, 1, 6);
+            }
+            else
+            {
+                startTime = new DateTime(2006, 1, 1);
+            }
+
+            ///转换为 Week Number 和 Seconds Of Week
+            TimeSpan Day_Diff = dateTime.Subtract(startTime);      //计算时间差值
+            int Weeks_Diff = Day_Diff.Days / 7 % 1024;             //计算周数差值，并基于1024周清零
+            int Week_Diff_Sec = Day_Diff.Days % 7 * 24 * 3600;     //计算余下天数对应的秒数
+
+            return Weeks_Diff + " weeks, " + Week_Diff_Sec + " s";
+        }
+
+        /// <summary>
+        /// 定义一个GNSS类型
+        /// </summary>
+        enum GNSS_Type
+        { 
+            GPS,    //GPS类型
+            BDS     //北斗
+        }
+    }
+}
---
+++ LKY_HW2_Coordinate_Conversion/Coordinate_Conversion.cs
@@ -0,0 +1,200 @@
+﻿/*
+ *      [Principle of Navigation] Copyright (C) 2022 SJTU Inc.
+ *      
+ *      FileName : Coordinate Conversion
+ *      Developer: liukaiyuan (Odysseus.Yuan)
+ */
+
+using System;
+
+namespace Coordinate_Conversion_LKY
+{
+    internal class Coordinate_Conversion
+    {
+        static void Main(string[] args)
+        {
+            //欢迎话术
+            Console.WriteLine("******** Homework #2: Coordinate Conversion  ********");
+            Console.WriteLine("******** Developer: Liu Kaiyuan              ********");
+            Console.WriteLine("******** Student Number: 122413930100        ********");
+            Console.WriteLine();
+
+            // -------------- Coordinate Conversion Begin --------------
+
+            Console.WriteLine("※※※ Part1: Coordinate conversion: find a point A on SJTU campus, marked on the Baidu/Google " +
+                "Map to obtain the geodetic coordinate(l,l,h), and convert it to ECEF, and ECI (optional).\n");
+            //定义交大空天院LLA
+            Coordinate.LLA sjtuA_lla;
+            sjtuA_lla.longitude = 121.44124568;
+            sjtuA_lla.latitude = 31.02546975;
+            sjtuA_lla.altitude = 5.26;
+
+            Console.WriteLine("选取位置A：上海交通大学（闵行校区）空天学院 门口：");
+            Console.WriteLine("----- > " + Coordinate.Type.LLA.ToString() + "  坐标：("
+                + sjtuA_lla.longitude.ToString() + ", " + sjtuA_lla.latitude.ToString() + ", " + sjtuA_lla.altitude.ToString() + ")");
+
+
+            //转为ECEF坐标
+            Coordinate.ECEF sjtuA_ecef = Coordinate.LLA2ECEF(sjtuA_lla);
+            Console.WriteLine("----- > " + Coordinate.Type.ECEF.ToString() + " 坐标：("
+                + sjtuA_ecef.x + ", " + sjtuA_ecef.y + ", " + sjtuA_ecef.z + ")");
+            ///ECEF基准值：-2857998.38, -1718988.29, 5418464.12
+
+            //转为ENU坐标(以天安门为基准)
+            Coordinate.LLA tam_lla;
+            tam_lla.longitude = 116.39127910;
+            tam_lla.latitude = 39.90713686;
+            tam_lla.altitude = 49.81;
+
+            Coordinate.ENU sjtuA_enu = Coordinate.ECEF2ENU(tam_lla, sjtuA_ecef);
+            Console.WriteLine("----- > " + Coordinate.Type.ENU.ToString() + "  坐标：("
+                + sjtuA_enu.East + ", " + sjtuA_enu.North + ", " + sjtuA_enu.Up + ") BY 天安门");
+            Console.WriteLine("----- > " + Coordinate.Type.ENU.ToString() + "  距离："
+                + (Coordinate.GetVectorLen(sjtuA_enu.East, sjtuA_enu.North, sjtuA_enu.Up)) + " m");
+
+
+
+            Console.WriteLine("\n\n※※※ Part2: Given a point B (121.455899°, 31.036321°，100m), " +
+                "calculate the ENU of point B relative to your own Point A. Plot the skymap marked with  elevation and azimuth. \n");
+            Console.WriteLine("转换位置B：(121.455899°, 31.036321°，100m)：");
+
+            //定义B点LLA坐标
+            Coordinate.LLA sjtuB_lla;
+            sjtuB_lla.longitude = 121.455899;
+            sjtuB_lla.latitude = 31.036321;
+            sjtuB_lla.altitude = 100d;
+
+            Coordinate.ENU sjtuB_enu = Coordinate.ECEF2ENU(sjtuA_lla, Coordinate.LLA2ECEF(sjtuB_lla));        //以A为目标，获取enu
+            Console.WriteLine("----- > " + Coordinate.Type.ENU.ToString() + " 坐标：("
+                + sjtuB_enu.East + ", " + sjtuB_enu.North + ", " + sjtuB_enu.Up + ") BY 位置A");
+            Console.WriteLine("----- > " + Coordinate.Type.ENU.ToString() + " 距离："
+                + (Coordinate.GetVectorLen(sjtuB_enu.East, sjtuB_enu.North, sjtuB_enu.Up)) + " m");
+
+            //获取SkyMap
+            double az = Math.Atan(sjtuB_enu.East / sjtuB_enu.North);
+            double el = Math.Asin(sjtuB_enu.Up / Math.Sqrt(Math.Pow(sjtuB_enu.East, 2) + Math.Pow(sjtuB_enu.North, 2) + Math.Pow(sjtuB_enu.Up, 2)));
+            Console.WriteLine("----- > " + "SkyMap  ：(" + "az: "
+                + az + "°, " + "el: " + el + "°)");
+
+
+            // -------------- Coordinate Conversion End --------------
+
+            Console.WriteLine("\n\nDone. 可按任意键退出运行。");
+            Console.ReadKey();
+        }
+
+
+
+    }
+
+    /// <summary>
+    /// 不同坐标系的定义
+    /// </summary>
+    class Coordinate
+    {
+        /// <summary>
+        /// 计算向量模长
+        /// </summary>
+        /// <returns></returns>
+        public static double GetVectorLen(double x, double y, double z)
+        {
+            double result = Math.Sqrt(x * x + y * y + z * z);
+
+            return result;
+        }
+
+        /// <summary>
+        /// LLA转ECEF
+        /// </summary>
+        /// <param name="lla"></param>
+        /// <returns></returns>
+        public static ECEF LLA2ECEF(LLA lla)
+        {
+            double a = 6378137.0;                //椭球体长半径
+            double f = 1.0 / 298.257223565;      //极扁率
+
+            double e2 = f * (2 - f);            //计算e²
+
+            //弧度制转换，非常重要
+            double deg2rad = Math.PI / 180.0;
+            double lat = lla.latitude * deg2rad;
+            double lon = lla.longitude * deg2rad;
+
+            double N = a / Math.Sqrt(1 - e2 * Math.Pow(Math.Sin(lat), 2));   //计算N
+
+            ECEF result;
+            result.x = (N + lla.altitude) * Math.Cos(lat) * Math.Cos(lon);
+            result.y = (N + lla.altitude) * Math.Cos(lat) * Math.Sin(lon);
+            result.z = (N * (1 - e2) + lla.altitude) * Math.Sin(lat);
+
+            return result;
+        }
+
+        /// <summary>
+        /// ECEF转ENU
+        /// 基于 byFromLLA 为基准点，将目标点的 ecef 转换为 ENU坐标
+        /// </summary>
+        /// <param name="ecef"></param>
+        /// <returns></returns>
+        public static ENU ECEF2ENU(LLA byFromLLA, ECEF ecef)
+        {
+            double deltaX = ecef.x - LLA2ECEF(byFromLLA).x;
+            double deltaY = ecef.y - LLA2ECEF(byFromLLA).y;
+            double deltaZ = ecef.z - LLA2ECEF(byFromLLA).z;
+
+            ///Console.WriteLine(deltaX.ToString() + ", " + deltaY.ToString() + ", " + deltaZ.ToString());
+
+            double lon = byFromLLA.longitude;
+            double lat = byFromLLA.latitude;
+
+
+            ENU result;
+            result.East = -Math.Sin(lon) * deltaX + Math.Cos(lon) * deltaY;
+            result.North = -Math.Sin(lat) * Math.Cos(lon) * deltaX - Math.Sin(lat) * Math.Sin(lon) * deltaY + Math.Cos(lat) * deltaZ;
+            result.Up = Math.Cos(lat) * Math.Cos(lon) * deltaX + Math.Cos(lat) * Math.Sin(lon) * deltaY + Math.Sin(lat) * deltaZ;
+
+            return result;
+        }
+
+        /// <summary>
+        /// 坐标系类型
+        /// </summary>
+        public enum Type
+        {
+            LLA,
+            ECEF,
+            ENU,
+            ECI
+        }
+
+        /// <summary>
+        /// LLA坐标系信息
+        /// </summary>
+        public struct LLA
+        {
+            public double longitude;  //经度
+            public double latitude;   //纬度
+            public double altitude;   //高度
+        }
+
+        /// <summary>
+        /// ECEF坐标系信息
+        /// </summary>
+        public struct ECEF
+        {
+            public double x;
+            public double y;
+            public double z;
+        }
+
+        /// <summary>
+        /// ENU坐标系信息
+        /// </summary>
+        public struct ENU
+        {
+            public double East;
+            public double North;
+            public double Up;
+        }
+    }
+}
